3538f4a7528b919442b9850a0055a8d9a145afe5
Calvin Yang <cyang2@ufl.edu>
2022-01-29 19:45:04 -0500

    please work
    yes

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-const httpServer = require("http");
+const express = require('express');
 const fs = require("fs");
 var path = require('path');
+const http = require('http');
+
+const app = express()
+const PORT= process.env.PORT || 3000; 
+const INDEX = path.join(__dirname, 'frontend');
+
+app.use(express.static(INDEX))
+app.use('*', (req, res) => {
+    res.sendFile(INDEX + '/index.html');
+});
+//app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const server = http.createServer(app);
+
 const { makeid } = require('./utils');
-const io = require('socket.io')(httpServer, {
+const io = require('socket.io')(server, {
     cors: {
         origin: '*',
         methods: ["GET", "POST"]
@@ -90,47 +104,46 @@ io.on('connection', client => {
     }
 });
 
-const PORT= process.env.PORT || 8080; 
 
-fs.readFile('./frontend/index.html', function (err, html) {
+// fs.readFile('./frontend/index.html', function (err, html) {
 
-    if (err) throw err;    
+//     if (err) throw err;    
 
-    httpServer.createServer(function(request, response) {  
-            var filePath = './frontend' + request.url;
-        if (filePath == './frontend/')
-            filePath = './frontend/index.html';
+//     httpServer.createServer(function(request, response) {  
+//             var filePath = './frontend' + request.url;
+//         if (filePath == './frontend/')
+//             filePath = './frontend/index.html';
 
-        var extname = path.extname(filePath);
-        var contentType = 'text/html';
-        switch (extname) {
-            case '.js':
-                contentType = 'text/javascript';
-                break;
-            case '.css':
-                contentType = 'text/css';
-                break;
-        }
-        fs.readFile(filePath, function(error, content) {
-            if (error) {
-                if(error.code == 'ENOENT'){
-                    fs.readFile('./404.html', function(error, content) {
-                        response.writeHead(200, { 'Content-Type': contentType });
-                        response.end(content, 'utf-8');
-                    });
-                }
-                else {
-                    response.writeHead(500);
-                    response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-                    response.end(); 
-                }
-            }
-            else {
-                response.writeHead(200, { 'Content-Type': contentType });
-                response.end(content, 'utf-8');
-            }
-        });
-    }).listen(PORT);
-});
+//         var extname = path.extname(filePath);
+//         var contentType = 'text/html';
+//         switch (extname) {
+//             case '.js':
+//                 contentType = 'text/javascript';
+//                 break;
+//             case '.css':
+//                 contentType = 'text/css';
+//                 break;
+//         }
+//         fs.readFile(filePath, function(error, content) {
+//             if (error) {
+//                 if(error.code == 'ENOENT'){
+//                     fs.readFile('./404.html', function(error, content) {
+//                         response.writeHead(200, { 'Content-Type': contentType });
+//                         response.end(content, 'utf-8');
+//                     });
+//                 }
+//                 else {
+//                     response.writeHead(500);
+//                     response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+//                     response.end(); 
+//                 }
+//             }
+//             else {
+//                 response.writeHead(200, { 'Content-Type': contentType });
+//                 response.end(content, 'utf-8');
+//             }
+//         });
+//     }).listen(PORT);
+// });
 
-io.listen(3000);
+server.listen(PORT);
